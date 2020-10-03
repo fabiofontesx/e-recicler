b2d0541eafab38107c8e0426a34e783fc512e919
fabiofontesx <fontes.fabio@outlook.com>
2020-10-03 16:19:30 -0300

    feat: started get user info on home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React, { useEffect, useState }  from 'react';
 import CardInfo from '../../components/CardInfo';
 
 import Container from '../../components/Container';
@@ -10,13 +10,44 @@ import Lixeira from '../../assets/lixeira.svg';
 import Dolar from '../../assets/dolar.svg';
 import Luminaria from '../../assets/luminaria.svg';
 import { useNavigation } from '@react-navigation/native';
+import { useAuth } from '../../contexts/AuthContext';
+import api from '../../services/api';
+
+interface IGarbageCollectTypes{
+    glass: number;
+    metal: number;
+    paper: number;
+    plastic: number
+}
+interface IUserGetDataResponse{
+    data: {
+        id: string;
+        collected_garbage: IGarbageCollectTypes
+        user: string;
+    }[]
+}
 
 const Home: React.FC = () => {
+    const { email } = useAuth();
     const navigation = useNavigation();
-
+    const [userGarbage, setUserGarbage] = useState<IGarbageCollectTypes>({} as IGarbageCollectTypes);
+    
     const goToNewDiscart = () => {
         navigation.navigate('NewDiscart');
     }
+
+    useEffect(() => {
+        const loadUserInfos = async () => {
+            try {
+                const response = await api.get(`garbcollect/user/${email}`);
+                console.log(response);
+            } catch (error) {
+                
+            }
+        }
+
+        loadUserInfos();
+    })
 
     return (
         <Container>
